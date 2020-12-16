cacf3267364aa90d8a3ddf08b9982527d38f36be
MAILLARDsam <samuel.maillard@siemens.com>
2020-12-16 15:38:02 +0100

    Ajout de commentaires dynamiques sur postit

--- JS/index.js
+++ JS/index.js
@@ -28,3 +28,27 @@ function formSubmited(evt) {
     console.log(evt.target[3].value);
 }
 
+/**
+ * 
+ * @param {String} titre titre de la note
+ * @param {String} date JJ:MM:AAAA
+ * @param {String} heure HH:MM
+ * @param {String} description description
+ */
+
+function createPostit(titre, date, heure, description) {
+    var postit = document.createElement('div');
+    // ajout d'un class dans la liste de classe d'un Element
+    postit.classList.add('postit');
+    // Possiblité de suppresion d'une class d'un balise
+    //ostit.classList.remove('postit');
+    //postit.innerHTML='Mon nouveau postit';
+
+    postit.innerHTML = '<div class="postit-titre">' + titre + '</div>\
+                date: <span class="datetime">'+ date + '</span> heure : <span class="datetime">' + heure + '</span>\
+                <h2>Description :</h2>'+ description;
+
+    //sélection de la liste de postit    
+    var liste = document.querySelector('#list');
+    liste.append(postit);
+}
