fdc52aa25675027f5f534f3a4d5ea9856f4903b9
MAILLARDsam <samuel.maillard@siemens.com>
2020-12-16 13:50:01 +0100

    modification du code JS

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,6 @@
-alert('le fichier est bien en place');
+// définition d'une variable et affectation d'un contenu
+var jsload=document.querySelector('#jsload');
+// modification du contenu html de la balise dans la var
+jsload.innerHTML='mon<span style="font-weight:900">JS</span> est chargé';
+// modification du style de la balise dans la var
+jsload.style.backgroundColor="LIME";
