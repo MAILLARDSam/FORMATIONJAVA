40094531669ca80e76038d0fe0f5ffda42a4211d
MAILLARDsam <samuel.maillard@siemens.com>
2020-12-16 14:51:47 +0100

    Fonction pour controler que le DOM est chargé

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,22 @@
+window.addEventListener('load', function (evt) {
+    //usage d'une fonction
+    initialisationJS('Samuel');
+    // Acrrochage d'un écouteur d'event sur une balise
+    // event : submit
+    // fonction à décelncher pour l'event -> formSubmit
+    document.querySelector('form').addEventListener('submit', formSubmited);
+});
+
 //declaration d'une fonction
 function initialisationJS(prenom) {
     // définition d'une variable et affectation d'un contenu
     var jsload = document.querySelector('#jsload');
     // modification du contenu html de la balise dans la var
-    jsload.innerHTML = 'mon<span style="font-weight:900">JS</span> est chargé';
+    jsload.innerHTML = 'mon<span style="font-weight:900">JS</span> est chargé' + prenom;
     // modification du style de la balise dans la var
     jsload.style.backgroundColor = "LIME";
 }
-//usage d'une fonction
-initialisationJS('Alexandre');
+
 
 function formSubmited(evt) {
     evt.preventDefault();
@@ -19,7 +27,4 @@ function formSubmited(evt) {
     console.log(evt.target[2].value);
     console.log(evt.target[3].value);
 }
-// Acrrochage d'un écouteur d'event sur une balise
-// event : submit
-// fonction à décelncher pour l'event -> formSubmit
-document.querySelector('form').addEventListener('submit',formSubmited)
+
